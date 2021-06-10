bd82e3aeed2133367a8da1aec0bc65ccbd978ff9
Enrique Coscarelli <enriquecoscarelli@gmail.com>
2021-06-10 11:31:28 +0200

    installed React template, installed mongoose and created the directory models with user.model file

---
+++ models/user.model.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: {type: String},
+  email: {type: String, unique: true},
+  password: {type: String},
+  userAgreement: {type: Boolean, default: false}
+
+}, {
+  timestamps: {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at'
+  },
+});
+
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
