c4eb1122e1e3201769e1cb277df3060f69942d3f
Enrique Coscarelli <enriquecoscarelli@gmail.com>
2021-06-15 17:18:11 +0200

    Added information to the product model schema

--- models/product.model.js
+++ models/product.model.js
@@ -7,36 +7,36 @@ const productSchema = new Schema({
   model: {type: String},
   img: {type: String},
   imgTecnica: {type: String},
-  pistillo:
-  fijo:
-  ajustable:
-  profunidad:
+  pistillo: {type: String},
+  fijo: {type: Boolean, default: false}
+  ajustable: {type: Boolean, default: false}
+  profunidad: {type: Number},
   ajuste:
   imgTecnicaPestillo: {type: String},
   bobina:
   tapa:
   functionresitenciaDeCierre
-  fuerzaDinamicaDeCierre:
-  durabilidadGarantizada:
-  minTemperaturaDeUso:
-  maxTemperaturaDeUso:
-  resistenciaAlFuego:
+  fuerzaDinamicaDeCierre: {type: Number},
+  durabilidadGarantizada: {type: Number},
+  minTemperaturaDeUso: {type: Number},
+  maxTemperaturaDeUso: {type: Number},
+  resistenciaAlFuego: {type: Number},
   certificadoSegunNorma:
   proteccionIP:
   apertura:
-  ancho:
-  largo:
-  alto:
+  ancho: {type: Number},
+  largo: {type: Number},
+  alto: {type: Number},
   caracteristicasElectricas:
-  voltajeAC:
-  voltajeDC:
-  funcionamientoContinuo:
-  tensionNominal:
-  protectorDeSobretension:
-  precargaMaximaDelPestilloAC:
-  precargaMaximaDelPestilloDC:
-  consumoDeCorrienteAC:
-  consumoDeCorrienteDC:
+  voltajeAC: {type: Number},
+  voltajeDC: {type: Number},
+  funcionamientoContinuo: {type: Number},
+  tensionNominal: {type: Number},
+  protectorDeSobretension: {type: Number},
+  precargaMaximaDelPestilloAC: {type: Number},
+  precargaMaximaDelPestilloDC: {type: Number},
+  consumoDeCorrienteAC: {type: Number},
+  consumoDeCorrienteDC: {type: Number},
   imgImpulsoElectrico: {type: String},
 
 
