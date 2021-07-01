e73469ff1d45ff58d1872be6599ba9da493c1555
Enrique Coscarelli <enriquecoscarelli@gmail.com>
2021-07-01 16:00:49 +0200

    Changed the scheema for the product a bit

--- models/product.model.js
+++ models/product.model.js
@@ -7,37 +7,47 @@ const productSchema = new Schema({
   model: {type: String},
   img: {type: String},
   imgTecnica: {type: String},
-  pistillo: {type: String},
+  
+  pestillo: {type: String},
   fijo: {type: Boolean, default: false}
   ajustable: {type: Boolean, default: false}
   profunidad: {type: Number},
   ajuste:
   imgTecnicaPestillo: {type: String},
+  
   bobina:
+  
   tapa:
-  functionresitenciaDeCierre
-  fuerzaDinamicaDeCierre: {type: Number},
-  durabilidadGarantizada: {type: Number},
-  minTemperaturaDeUso: {type: Number},
-  maxTemperaturaDeUso: {type: Number},
-  resistenciaAlFuego: {type: Number},
-  certificadoSegunNorma:
-  proteccionIP:
-  apertura:
-  ancho: {type: Number},
-  largo: {type: Number},
-  alto: {type: Number},
-  caracteristicasElectricas:
-  voltajeAC: {type: Number},
-  voltajeDC: {type: Number},
-  funcionamientoContinuo: {type: Number},
-  tensionNominal: {type: Number},
-  protectorDeSobretension: {type: Number},
-  precargaMaximaDelPestilloAC: {type: Number},
-  precargaMaximaDelPestilloDC: {type: Number},
-  consumoDeCorrienteAC: {type: Number},
-  consumoDeCorrienteDC: {type: Number},
-  imgImpulsoElectrico: {type: String},
+  
+  resistenteAlFuego: {type: Boolean, default: false};
+
+  funcion:
+  
+  // manualInstructivo: {type: String},
+  
+  // resitenciaDeCierre:
+  // fuerzaDinamicaDeCierre: {type: Number},
+  // durabilidadGarantizada: {type: Number},
+  // minTemperaturaDeUso: {type: Number},
+  // maxTemperaturaDeUso: {type: Number},
+  // resistenciaAlFuego: {type: Number},
+  // certificadoSegunNorma:
+  // proteccionIP:
+  // apertura:
+  // ancho: {type: Number},
+  // largo: {type: Number},
+  // alto: {type: Number},
+  // caracteristicasElectricas:
+  // voltajeAC: {type: Number},
+  // voltajeDC: {type: Number},
+  // funcionamientoContinuo: {type: Number},
+  // tensionNominal: {type: Number},
+  // protectorDeSobretension: {type: Number},
+  // precargaMaximaDelPestilloAC: {type: Number},
+  // precargaMaximaDelPestilloDC: {type: Number},
+  // consumoDeCorrienteAC: {type: Number},
+  // consumoDeCorrienteDC: {type: Number},
+  // imgImpulsoElectrico: {type: String},
 
 
 
