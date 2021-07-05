2f6cb1e36b1fd9f3316a9692a5a509d005bbbc91
Enrique Coscarelli <enriquecoscarelli@gmail.com>
2021-07-05 17:24:04 +0200

    debugging products.model

--- models/product.model.js
+++ models/product.model.js
@@ -1,19 +1,11 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const electricosSchema = require('../schemas/electricos.schema')
+// const electricosSchema = require('../schemas/electricos.schema')
 
 const productSchema = new Schema({
   category: {type: String},
-  series: {type: String},
-  model: {type: String, unique: true},
-  descripcion: { type: String},
-  imgProducto: {type: String},
-  imgTecnica: {type: String},
-  manualInstructivo: {type: String},
-  ancho: {type: Number},
-  largo: {type: Number},
-  alto: {type: Number},
+  
 
   // fichaTecnica: { type: [ electricosSchema ], required: true }
 }, {
